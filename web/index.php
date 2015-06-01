4ce50c1038ba890624edc30745b4e433a0c68c2d
mile <mile@Mile-Laptop>
2015-06-01 21:47:32 +0200

    Initial Comit

--- web/index.php
+++ web/index.php
@@ -1,12 +1,23 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == 'vital.dev') {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+    require(__DIR__ . '/../vendor/autoload.php');
+    require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+    $config = require(__DIR__ . '/../config/web-local.php');
 
-(new yii\web\Application($config))->run();
+    (new yii\web\Application($config))->run();
+} else {
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+
+    require(__DIR__ . '/../vendor/autoload.php');
+    require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+    $config = require(__DIR__ . '/../config/web.php');
+
+    (new yii\web\Application($config))->run();
+}
