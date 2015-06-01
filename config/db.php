696306e770ca74c57c39cc357a7a5848207e3918
mile <mile@Mile-Laptop>
2015-06-01 21:10:14 +0200

    Initial Comit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=vital',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
