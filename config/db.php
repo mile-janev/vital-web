d7a356efc88759ec68794f8877b2c2448a6f9d98
mile <mile@Mile-Laptop>
2015-06-01 22:24:12 +0200

    Initial Comit

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vital',
-    'username' => 'root',
-    'password' => 'toor',
+    'dsn' => 'mysql:host=localhost;dbname=simyanin_vital',
+    'username' => 'simyanin_vital',
+    'password' => 'S1my@n!n_vital',
     'charset' => 'utf8',
 ];
