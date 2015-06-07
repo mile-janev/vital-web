8076179e08d1dd1c0b79a0ede03b0a0081540772
mile <mile@Mile-Laptop>
2015-06-07 23:59:10 +0200

    Roles, main menu design

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -1,31 +1,40 @@
 <?php
-            NavBar::begin([
-                'brandLabel' => 'Healthcare Record System',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top main-menu-nav',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right main-menu'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    [
-                        'label' => 'Site',
-                        'items' => [
-                            ['label' => 'About', 'url' => Url::toRoute("site/about")],
-                            ['label' => 'Home', 'url' => Url::toRoute("site/index")],
-                        ],
-                    ],
-                    Yii::$app->user->isGuest ? ['label' => 'Register', 'url' => ['/site/register']] : "",
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->email . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
-            NavBar::end();
-        ?>
+
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\helpers\Html;
+use yii\helpers\Url;
+?>
+
+<?php
+
+NavBar::begin([
+    'brandLabel' => 'Healthcare Record System',
+    'brandUrl' => Yii::$app->homeUrl,
+    'options' => [
+        'class' => 'navbar-inverse navbar-fixed-top main-menu-nav',
+    ],
+]);
+echo Nav::widget([
+    'options' => ['class' => 'navbar-nav navbar-right main-menu'],
+    'items' => [
+        ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'Contact', 'url' => ['/site/contact']],
+        [
+            'label' => 'Site',
+            'items' => [
+                ['label' => 'About', 'url' => Url::toRoute("site/about")],
+                ['label' => 'Home', 'url' => Url::toRoute("site/index")],
+            ],
+        ],
+        Yii::$app->user->isGuest ? ['label' => 'Register', 'url' => ['/site/register']] : "",
+        Yii::$app->user->isGuest ?
+                ['label' => 'Login', 'url' => ['/site/login']] :
+                ['label' => 'Logout (' . Yii::$app->user->identity->email . ')',
+            'url' => ['/site/logout'],
+            'linkOptions' => ['data-method' => 'post']],
+    ],
+]);
+NavBar::end();
+?>
