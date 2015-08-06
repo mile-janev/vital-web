838c79353622c5a9ec7a6e39342003b818db8cd3
Mile Yanev <mile.janev@simyan.info>
2015-08-07 01:13:45 +0200

    Pretty Urls

--- config/web-local.php
+++ config/web-local.php
@@ -38,6 +38,18 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db-local.php'),
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            //'enableStrictParsing' => true,
+            //'suffix' => '.html',
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ],
+        ],
     ],
     'params' => $params,
 ];
