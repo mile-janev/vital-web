392ab5d53ba47fc6d11909404e7e1ade9da5057f
Mile Yanev <mile.janev@simyan.info>
2015-12-02 02:06:41 +0100

    MEWS

--- views/user/patient_mews.php
+++ views/user/patient_mews.php
@@ -1,6 +1,8 @@
 <?php
+    use yii\helpers\Html;
     use app\models\User;
     use app\components\Functions;
+    use yii\bootstrap\ActiveForm;
 
     $this->title = $user->name;
     $mews = app\models\User::mews($user->id);
@@ -9,9 +11,58 @@
 ?>
 
 <div class="patient-mews container">
-
-    <div class="patient-mews">
-        Comming soon!
+    
+    <div class="row">
+        <div class="col-xs-12 text-center">
+            <h1 id="mews-title">Calculate MEWS for <?= Html::encode($this->title) ?></h1>
+        </div>
     </div>
 
+    <?php $form = ActiveForm::begin(['id' => 'mews-form', 'action' => \yii\helpers\Url::toRoute(['user/mews-validate'])]);?>
+
+        <?= $form->field($model, 'patient')->hiddenInput(['value' => $user->id])->label(false) ?>
+    
+    <div class="row">
+        
+        <div class="col-xs-12 col-sm-4">
+            <?= $form->field($model, 'systolic')->textInput() ?>
+        </div>
+        
+        <div class="col-xs-12 col-sm-4">
+            <?= $form->field($model, 'heart')->textInput() ?>
+        </div>
+    
+        <div class="col-xs-12 col-sm-4">
+            <?= $form->field($model, 'respiratory')->textInput() ?>
+        </div>
+    </div>
+    
+    <div class="row">
+        
+        <div class="col-xs-12 col-sm-4">
+            <?= $form->field($model, 'temperature')->textInput() ?>
+        </div>
+
+        <div class="col-xs-12 col-sm-4">
+            <?= $form->field($model, 'avpu')->dropDownList(Functions::avpu()) ?>
+        </div>
+        
+        <div class="col-xs-12 col-sm-4">
+            <input id="mews-button" class="btn btn-primary" type="submit" name="Calculate" value="Calculate" />
+            <div id="mews-save-wrapper">
+                <a id="mews-save" href="<?= \yii\helpers\Url::toRoute(["user/mews-save"]) ?>">Save Measurements</a>
+                <div id="mews-saved">Measurements Saved</div>
+            </div>
+        </div>
+        
+    </div>
+    
+    <div class="row">
+        <div class="col-xs-12">
+            <div id="mews-value">0</div>
+        </div>
+    </div>
+    
+    <?php ActiveForm::end(); ?>
+    
 </div>
